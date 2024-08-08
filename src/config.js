11c732aada9a4d8fa16d1306b3d8aef73d4797dd
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-08 20:00:00 +0100

    config is loaded from userData as filesystem is compressed and cannot be edited when in production

--- src/config.js
+++ src/config.js
@@ -1,15 +1,14 @@
+import { app } from "electron";
 import fs from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
-// Directory of the this current file
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { join } from "path";
 
 // Name of the configuration file
 const CONFIG_NAME = "config.json";
 
+// Load config from external file
 export function getConfig() {
-	const configPath = join(__dirname, CONFIG_NAME);
+	// Read the file and return the JSON as an object
+	const configPath = join(app.getPath("userData"), CONFIG_NAME);
 	const appConfigRaw = fs.readFileSync(configPath, { encoding: "utf-8" });
 
 	return JSON.parse(appConfigRaw);
