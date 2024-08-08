f8a17c1a6233fcc5039d02a7fcceea92aa860f9e
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-08 20:08:56 +0100

    creates the config file if it does not already exist

--- src/config.js
+++ src/config.js
@@ -5,11 +5,21 @@ import { getPathDev } from "./utils.js";
 // Name of the configuration file
 const CONFIG_NAME = "config.json";
 
-// Load config from external file
-export function getConfig() {
-	// Read the file and return the JSON as an object
-	const configPath = join(getPathDev("userData"), CONFIG_NAME);
-	const appConfigRaw = fs.readFileSync(configPath, { encoding: "utf-8" });
+const DEFAULT_CONFIG = {
+	display: "",
+};
 
+// Load config from external file, the file will be created if it does not already exist
+export function getConfig() {
+	const configPath = join(getPathDev("userData"), CONFIG_NAME);
+
+	// If the file does not exist then create it
+	if (!fs.existsSync(configPath)) {
+		fs.writeFileSync(configPath, JSON.stringify(DEFAULT_CONFIG));
+		return DEFAULT_CONFIG;
+	}
+
+	// Read the file and return the JSON as an object
+	const appConfigRaw = fs.readFileSync(configPath, { encoding: "utf-8" });
 	return JSON.parse(appConfigRaw);
 }
