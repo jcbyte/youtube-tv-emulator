856f88322116644ad4e7a0a296955510853a7338
jcbyte <57547887+jcbyte@users.noreply.github.com>
2024-08-08 17:12:05 +0100

    when installing with squirrel, a default installation steps are now performed (like shortcut creation)

--- forge.config.js
+++ forge.config.js
@@ -8,6 +8,11 @@ module.exports = {
 			name: "@electron-forge/maker-squirrel",
 			config: {
 				setupIcon: "./assets/icon.ico",
+				// `iconUrl` is to set the icon which shows up in control panel and can only be remote files,
+				// (this is annoying as if the user installs without internet it will not be changed).
+				// Note: you must also restart the system before this icon will show up in the control panel.
+				// ! In the future this may be changed to allow local files, if so this should be updated to use that.
+				iconUrl: "https://raw.githubusercontent.com/jcbyte/youtube-tv-emulator/main/assets/icon.ico",
 			},
 		},
 		{
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+if (require("electron-squirrel-startup")) return;
+
 function createWindow() {
 	// Create a new window
 	win = new BrowserWindow({
